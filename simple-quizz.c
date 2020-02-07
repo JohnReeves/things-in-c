26c4b172a202863e51f138a20413f80eb1742bd4
John Reeves <JohnReeves@users.noreply.github.com>
2020-02-07 15:31:16 +0000

    Update simple-quizz.c

--- simple-quizz.c
+++ simple-quizz.c
@@ -14,17 +14,27 @@ question quizz[5] = {\
 };
 
 int main(){
-    printf("hello\n");
     char * answer;
+    char name[10];
+    int score = 0;
+    int out_of = sizeof(quizz)/sizeof(quizz[0]);
 
-    for (int i = 0; i < 5; i++){
-        printf("What is the capital of %s ?\n >>", quizz[i].q);
+    printf("type in your name: ");
+    fgets(name,10,stdin);
+    printf("hello %s\n", name);
+
+    for (int i = 0; i < out_of; i++){
+        printf("What is the capital of %s?\n >>", quizz[i].q);
         fgets(answer,10,stdin);
         printf("%s \n", answer);
         if (answer == quizz[i].a){
             printf("that's correct!\n");
+            score++;
         } else {
             printf("that's wrong!\n");
         }
    }
+
+   printf("You scored: %d out of %d", score, out_of);
+   return 0;
 }
