93e5dc02c2ae169fafd1fddb8cd1f94da7b1b0e9
John Reeves <johnreeves.mathsandcomputing@gmail.com>
2020-02-07 16:25:43 +0000

    fgets instead of scanf

--- simple-quizz.c
+++ simple-quizz.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 typedef struct {
 char * q;
@@ -14,20 +15,21 @@ question quizz[5] = {\
 };
 
 int main(){
-    char answer[10];
+    char * answer;
     char name[10];
     int score = 0;
     int out_of = sizeof(quizz)/sizeof(quizz[0]);
 
     printf("type in your name: ");
-    fgets(name,10,stdin);
-    printf("hello %s\n", name);
+    fgets(name, 10, stdin);    
+
+    printf("hello %s There will be %d questions\n", name, out_of);
 
     for (int i = 0; i < out_of; i++){
-        printf("What is the capital of %s?\n >>", quizz[i].q);
-        fgets(answer,10,stdin);
-        printf("%s \n", answer);
-        if (answer == quizz[i].a){
+        printf("What is the capital of %s?\n>>> ", quizz[i].q);
+        scanf("%s",answer);
+
+        if (strcmp(answer,quizz[i].a) == 0){
             printf("that's correct!\n");
             score++;
         } else {
@@ -35,6 +37,6 @@ int main(){
         }
    }
 
-   printf("You scored: %d out of %d", score, out_of);
+   printf("You scored: %d out of %d\n", score, out_of);
    return 0;
 }
