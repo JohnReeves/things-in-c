0bf064b5c2531e1e683361ddb2a16713771a02fb
John Reeves <johnreeves.mathsandcomputing@gmail.com>
2020-02-12 13:49:27 +0000

    match code to algo

--- simple-rpn-calc.cpp
+++ simple-rpn-calc.cpp
@@ -39,9 +39,19 @@ int main() {
     sregex_iterator it(instr.begin(), instr.end(), re);
     sregex_iterator it_end;
 
+    // for each token in the postfix expression:
     for (;it != it_end; ++it) process_token(it->str()){
+
+      //  if token is an operand:
+      //    push token onto the stack
       if (s.find_first_not_of("+*/-") != s.npos) {
         st.push(atof(s.c_str()));
+
+      //  else if token is an operator:
+      // operand_2 ← pop from the stack
+      // operand_1 ← pop from the stack
+      // result ← evaluate token with operand_1 and operand_2
+      // push result back onto the stack
       } else {
         double op2 = st.top(); st.pop();
         double op1 = st.top(); st.pop();
