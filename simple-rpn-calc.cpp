211f31109956e364dc585cecb3a3eb9495a63c35
John Reeves <johnreeves.mathsandcomputing@gmail.com>
2020-02-12 13:45:39 +0000

    single main function to match algo

--- simple-rpn-calc.cpp
+++ simple-rpn-calc.cpp
@@ -20,53 +20,40 @@ result ← pop from the stack
 
 using namespace std;
 
-//using std::cout;         // Alternatively, you can use
-//using std::cin;          //  using namespace std;
-//using std::endl;
-//using std::string;
-//using std::regex;
-//using std::sregex_iterator;
-//using std::stack;
-
 void process_token(string s);
+
 stack<double> st;
+string instr;
+string num_pattern("[0-9]+(\\.[0-9]*)?");
+string op_pattern("[+*/-]");
+regex re(num_pattern + "|" + op_pattern);
 
-     string instr;
-     string num_pattern("[0-9]+(\\.[0-9]*)?");
-     string op_pattern("[+*/-]");
-     regex re(num_pattern + "|" + op_pattern);
-     
-main() {
+int main() {
 
-     while (true) {
-         cout << "Enter expression (or ENTER to exit): ";
-         getline(cin, instr);
-         if (instr.length() == 0) { break; }
-         sregex_iterator it(instr.begin(), instr.end(), re);
-         sregex_iterator it_end;
-         for (;it != it_end; ++it) {
-              process_token(it->str());
-         }
-         if (!st.empty()) {
-              cout << "The value is: " << st.top() << endl;
-         }
-     };
-     return 0;
-}
+  while (true) {
+    cout << "Enter expression (or ENTER to exit): ";
+    getline(cin, instr);
 
-void process_token(string s) {
-     // If s contains any char that is NOT an op,
-     //  consider it a number by default.
-     if (s.find_first_not_of("+*/-") != s.npos) {
-          st.push(atof(s.c_str()));
-     } else {
-          double op2 = st.top(); st.pop();
-          double op1 = st.top(); st.pop();
-          switch(s[0]) {
+    if (instr.length() == 0) break;
+
+    sregex_iterator it(instr.begin(), instr.end(), re);
+    sregex_iterator it_end;
+
+    for (;it != it_end; ++it) process_token(it->str()){
+      if (s.find_first_not_of("+*/-") != s.npos) {
+        st.push(atof(s.c_str()));
+      } else {
+        double op2 = st.top(); st.pop();
+        double op1 = st.top(); st.pop();
+        switch(s[0]) {
           case '+': st.push(op1 + op2); break;
           case '-': st.push(op1 - op2); break;
           case '*': st.push(op1 * op2); break;
           case '/': st.push(op1 / op2); break;
-          }
-     }
+        }
+      }
+    }
+    if (!st.empty()) cout << "The value is: " << st.top() << endl;
+  }
+  return 0;
 }
